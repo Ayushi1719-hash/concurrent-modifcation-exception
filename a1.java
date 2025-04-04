43456887ed1821d8c632b4b8a26ce75316d8846a
Ayushi Sinha <Ayushi.Sinha@bounteous.com>
2025-04-04 12:08:20 +0530

    Update a1.java

--- a1.java
+++ a1.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
-
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
+        // Creates a reader instance which takes input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
 
         // nextInt() reads the next integer from the keyboard
         int number = reader.nextInt();
 
-        // println() prints the following line to the output screen
+        // Display user input
         System.out.println("You entered: " + number);
+
+        // New addition: square the input and print it
+        int square = number * number;
+        System.out.println("Square of the number is: " + square);
+
+        // New addition: say hello
+        System.out.println("Hello from Java!");
     }
 }
